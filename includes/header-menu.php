daeb4967de16e46b366db9347599f4cf3347b822
JM Maguigad <jmmaguigad@users.noreply.github.com>
2018-07-07 17:01:08 +0800

    Update file
    Updated header-menu.php

--- includes/header-menu.php
+++ includes/header-menu.php
@@ -1,7 +1,7 @@
 <div class="btn-holder">
   <a class="btn btn-success" title="Spark Pool" href="index.php">Spark Pool</a>
-  <a class="btn btn-success" title="Ethermine" href="index.php">Ethermine</a>
-  <a class="btn btn-success" title="Ethosdistro" href="index.php">Ethosdistro</a>
+  <a class="btn btn-success" title="Ethermine" href="ethermine.php">Ethermine</a>
+  <a class="btn btn-success" title="Ethosdistro" href="ethos.php">Ethosdistro</a>
 </div>
 <div class="clearfix"></div>
 <strong>Page will update every 20 minutes</strong>
