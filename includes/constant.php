2f2c18f4f039b67711d64f135b90701c495f050f
JM Maguigad <jmmaguigad@users.noreply.github.com>
2018-07-07 16:28:15 +0800

    Updated constant.php
    Updated comment on constant.php

--- includes/constant.php
+++ includes/constant.php
@@ -1,3 +1,3 @@
 <?php
-define('WALLET_ADDRESS','PUT_YOUR_WALLET_ADDRESS_HERE'); //Change this to your wallet address, used to access your ethermine account
+define('WALLET_ADDRESS','PUT_YOUR_WALLET_ADDRESS_HERE'); //Change this to your wallet address, used to access your ethermine and sparkpool account
 define('DS','/'); //Directory Separator
